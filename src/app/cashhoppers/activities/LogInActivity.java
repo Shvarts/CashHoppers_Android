b2cfb77171fab502af88dc12333cdd2e4cece75c
LuckyFenix <garapkor@gmail.com>
2013-09-14 19:03:30 +0300

    Log In activity

---
+++ src/app/cashhoppers/activities/LogInActivity.java
@@ -0,0 +1,14 @@
+package app.cashhoppers.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LogInActivity extends Activity
+{
+	@Override
+    protected void onCreate(Bundle savedInstanceState) 
+	{
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in_activity);
+    }
+}
