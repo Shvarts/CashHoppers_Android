3f37719fa1c0e1b371667c6c60729485fd13001c
Valsor7 <yaroslav0409@gmail.com>
2013-09-15 23:58:22 +0300

    Reset_password_activity

--- src/app/cashhoppers/activities/HomePageActivity.java
+++ src/app/cashhoppers/activities/HomePageActivity.java
@@ -2,22 +2,23 @@ package app.cashhoppers.activities;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class HomePageActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        
+    	super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home_page, menu);
         return true;
     }
-    
 }
---
+++ src/app/cashhoppers/activities/ResetPassActivity.java
@@ -0,0 +1,13 @@
+package app.cashhoppers.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ResetPassActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_reset_pass);
+		
+	}
+}
